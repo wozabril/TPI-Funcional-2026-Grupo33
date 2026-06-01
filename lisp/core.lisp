f8545196641c6e61f0d8d6201aa8e41ad0e4b222
wozabril <wozabril@gmail.com>
2026-06-01 12:30:21 -0300

    Actualizacion core.lisp
    Se modifica el formato de la salida de la funcion transicion (reemplazo de list por format)

--- lisp/core.lisp
+++ lisp/core.lisp
@@ -4,16 +4,32 @@
 ;; IMPACTO: No destructiva
 
 (Defun valido-datos (color-actual color-siguiente)
+     (IF (and (or (equalp color-actual 'en-rojo)
+                  (equalp color-actual 'en-amarillo) 
+                  (equalp color-actual 'en-verde)) 
+                  
+              (or (equalp color-siguiente 'rojo) 
+              (equalp color-siguiente 'amarillo) 
+              (equalp color-siguiente 'verde))
 
-    (IF (and (or (equalp color-actual 'en-rojo) (equalp color-actual 'en-amarillo) 
-                 (equalp color-actual 'en-verde)) (or (equalp  color-siguiente 'rojo) 
-                 (equalp color-siguiente 'amarillo) (equalp color-siguiente 'verde)) 
-                 (not (equalp color-actual color-siguiente)) (transicion color-actual color-siguiente) "Error en el ingreso de datos"))
+              (and (not (and (equalp color-actual 'en-rojo) (equalp color-siguiente 'rojo)) 
+              (not (and (equalp color-actual 'en-amarillo) (equalp color-siguiente 'amarillo)) 
+              (not (and (equalp color-actual 'en-verde) (equalp color-siguiente 'verde)))))
+              
+(transicion color-actual color-siguiente) "Error en el ingreso de datos"))))
+
+ 
 
 ;; FUNCION: transición 
 ;; NATURALEZA: Pura (muestra el color actual del semaforo y a que color cambia)
 ;; ESTRATEGIA: Orden superior (muestra el color actual del semaforo y a que color cambia)
 ;; IMPACTO: No destructiva
 
-(Defun transición (color-actual color-siguiente)
-    (List color-actual "cambiar-a" color-siguiente))
+(defun transicion (color_actual color_siguiente)
+    (COND 
+        ((AND (EQUALP COLOR_ACTUAL 'en_rojo) (equalp color_siguiente 'amarillo)) (format t "~a pasar-a~a" color_actual color_siguiente))
+        ((AND (EQUALP COLOR_ACTUAL 'en_amarillo) (equalp color_siguiente 'verde)) (format t "~a pasar-a~a" color_actual color_siguiente))
+        ((AND (EQUALP COLOR_ACTUAL 'en_verde) (equalp color_siguiente 'amarillo)) (format t "~a pasar-a~a" color_actual color_siguiente))
+        ((AND (EQUALP COLOR_ACTUAL 'en_amarillo) (equalp color_siguiente 'ro)) (format t "~a pasar-a~a" color_actual color_siguiente))   
+    )
+)
