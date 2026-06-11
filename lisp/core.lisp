bd79ccf612f066728298fab84243f22a3a15fc43
Fernando_Silva_673 <fermnandosilva673n@gmail.com>
2026-06-11 06:23:20 -0300

    actualiazacion con mapcar de la funcion

--- lisp/core.lisp
+++ lisp/core.lisp
@@ -1,14 +1,20 @@
 
 ;; REQUERIMIENTO 6
 ;; FUNCION: distribucion-temporal
-;; NATURALEZA: Impura (imprime información en pantalla mediante FORMAT)
-;; ESTRATEGIA: Construcción directa (calcula los porcentajes mediante fórmulas aritméticas sin recursión ni funciones de orden superior)
-;; IMPACTO: No destructiva (no modifica estructuras ni variables)
+;; NATURALEZA: Impura/efectos secundarios (imprime información en pantalla mediante FORMAT)
+;; ESTRATEGIA: Función de orden superior (utiliza MAPCAR para calcular los porcentajes de cada color)
+;; IMPACTO: No destructiva
 
 (defun distribucion-temporal ()
 
-  (format t
-          "Rojo: ~,2F%~%Amarillo: ~,2F%~%Verde: ~,2F%~%"
-          (* (/ 90.0 216) 100)
-          (* (/ 6.0 216) 100)
-          (* (/ 120.0 216) 100)))
+  (let ((porcentajes
+          (mapcar
+           #'(lambda (segundos)
+               (* (/ segundos 216.0) 100))
+           '(90 6 120))))
+
+    (format t
+            "Rojo: ~,2F%~%Amarillo: ~,2F%~%Verde: ~,2F%~%"
+            (first porcentajes)
+            (second porcentajes)
+            (third porcentajes))))
