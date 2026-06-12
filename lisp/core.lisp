b73e00b3ffdc9db417a6c59ced5eea7b6e52f6f5
Fernando_silva_673 <fernandosilva673n@gmail.com>
2026-06-12 04:37:56 -0300

    actuaizacion de distribucion de datos

--- lisp/core.lisp
+++ lisp/core.lisp
@@ -134,3 +134,24 @@
        (nth-value 0
          (floor(* minutos 60)216))
        "ERROR EN EL INGRESO DE DATOS"))
+
+
+;; REQUERIMIENTO 6
+;; FUNCION: distribucion-temporal
+;; NATURALEZA: Impura/efectos secundarios (imprime información en pantalla mediante FORMAT)
+;; ESTRATEGIA: Función de orden superior (utiliza MAPCAR para calcular los porcentajes de cada color)
+;; IMPACTO: No destructiva
+
+(defun distribucion-temporal ()
+
+  (let ((porcentajes
+          (mapcar
+           #'(lambda (segundos)
+               (* (/ segundos 216.0) 100))
+           '(90 6 120))))
+
+    (format t
+            "Rojo: ~,2F%~%Amarillo: ~,2F%~%Verde: ~,2F%~%"
+            (first porcentajes)
+            (second porcentajes)
+            (third porcentajes))))
