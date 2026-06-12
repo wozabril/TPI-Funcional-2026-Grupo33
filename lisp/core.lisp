2129e4f9109d0de96a4c738dbc68d8219e82d5b4
Fernando_silva_673 <fernandosilva673n@gmail.com>
2026-06-12 04:54:34 -0300

    actualizacion de aseguramientos de calidad

--- lisp/core.lisp
+++ lisp/core.lisp
@@ -155,3 +155,30 @@
             (first porcentajes)
             (second porcentajes)
             (third porcentajes))))
+
+;; REQUERIMIENTO 7
+;; FUNCION: ASEGURAMIENTO DE LA CALIDAD
+;; NATURALEZA: Impura (ejecuta funciones que pueden producir salida por pantalla)
+;; ESTRATEGIA: Validación mediante casos de prueba representativos de uso normal, alternativo y erróneo
+;; IMPACTO: No destructiva
+
+;; REQ 1
+(valido-datos 'en-rojo 'amarillo)
+(valido-datos 'en-verde 'amarillo)
+(valido-datos 'azul 'verde)
+
+;; REQ 2
+(siesentero 100)
+(siesentero "hola")
+
+;; REQ 3
+(cambio-estado-luz 90)
+(cambio-estado-luz 50)
+
+;; REQ 4
+(ingreso-ciclo 216)
+(ingreso-ciclo "abc")
+
+;; REQ 5
+(ciclos-por-tiempo 15)
+(ciclos-por-tiempo -5)
